df3f4549e89cceb864c56f2a5324c779fb1ca16d
DurgeshY21 <durgeshy5226@gmail.com>
2024-10-24 14:26:59 +0530

    adding logout

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,9 +1,17 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/SpotLogo.png";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Navbar = () => {
-  const walletAddress = useSelector((state) => state.wallet.address);
+  const dispatch = useDispatch();
+  const dataArray = useSelector((state) => state?.wallet?.dataObject);
+
+  // Function to handle sign out
+  const handleSignOut = () => {
+    dispatch(setDataObject()); // Clear wallet data using dispatch
+    toast.success("Signed out successfully");
+  };
+
   return (
     <div className="bg-[#151515] flex flex-row justify-between items-center px-6 md:px-8 lg:px-16 2xl:px-24 p-4">
       <img src={logo} alt="" className="" />
@@ -14,8 +22,9 @@ const Navbar = () => {
             type="button"
             className="whitespace-nowrap bg-[linear-gradient(to_right,#FFE27A,#FFBA57,#98DB7C,#8BCAFF)] text-black font-bold py-3  px-8 md:px-6 rounded-full
              shadow-lg hover:shadow-xl transition-all w-full "
+            onClick={dataArray ? handleSignOut : null}
           >
-            {walletAddress ? walletAddress : "Login"}
+            {dataArray ? "Logout" : "Login"}
           </button>
         </div>
       </Link>
