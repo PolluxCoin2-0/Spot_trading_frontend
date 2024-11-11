9caafc05ce877350c40ee0fc864a57c262c59073
shubhampoxscan <shubham.gupta@poxscan.io>
2024-11-11 17:27:03 +0530

    code pushed

--- src/component/MatrixCard.jsx
+++ src/component/MatrixCard.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { getMatrixRecordsApi } from '../utils/axios/apisFunction';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { getMatrixRecordsApi } from "../utils/axios/apisFunction";
+import { useSelector } from "react-redux";
 
 const MatrixCard = () => {
   const [data, setData] = useState({});
@@ -10,9 +10,9 @@ const MatrixCard = () => {
     const fetchData = async () => {
       try {
         const matrixRecordData = await getMatrixRecordsApi(
-            dataArray?.[0]?.userAddress,
-          );
-          console.log(matrixRecordData)
+          dataArray?.[0]?.userAddress
+        );
+        console.log(matrixRecordData);
         if (matrixRecordData.status_code === "1") {
           setData(matrixRecordData.data);
         }
@@ -26,38 +26,47 @@ const MatrixCard = () => {
 
   return (
     <div className="">
-        <div
-          key={data._id}
-          className=" rounded-2xl shadow-lg p-6 transition duration-300"
-        >
-          <h2 className="text-lg font-semibold text-white">
-            Level {data.level}
-          </h2>
-          <p className="text-[#868386] font-semibold text-sm">User ID: {data.user}</p>
+      <div
+        key={data._id}
+        className=" rounded-2xl shadow-lg p-6 transition duration-300"
+      >
+        <h2 className="text-lg font-semibold text-white">Slot {data.level}</h2>
+        <p className="text-white font-semibold text-sm">User ID: {data.user}</p>
 
-          <div className="mt-4">
-            <p
-              className={`font-bold text-sm px-2 py-1 rounded ${
-                data.isSlotFilled ? ' text-green-600' : ' text-red-600'
+        <div className="mt-4">
+          <p
+            className={`font-bold text-base py-1 rounded ${
+              data.isSlotFilled ? " text-green-600" : " text-red-600"
+            }`}
+          >
+            {data.isSlotFilled ? "Slot Filled" : "Slot Open"}
+          </p>
+        </div>
+        <p className="mt-4 text-white font-semibold text-sm">
+          Parent: {data.parent}
+        </p>
+
+        <div className="mt-4 flex justify-between text-sm text-gray-600">
+          <div className="flex flex-col items-center">
+            <span
+              className={`font-semibold rounded-full h-16 w-16 flex items-center justify-center ${
+                data.left ? "bg-green-500 text-white" : "bg-red-500 text-white"
               }`}
             >
-              {data.isSlotFilled ? 'Slot Filled' : 'Slot Open'}
-            </p>
+              1
+            </span>
           </div>
-
-          <div className="mt-4 flex justify-between text-sm text-gray-600">
-            <div className="flex flex-col items-center">
-              <span className="text-[#868386] font-semibold">Left</span>
-              <span>{data.left ?? 'N/A'}</span>
-            </div>
-            <div className="flex flex-col items-center">
-              <span className="text-[#868386] font-semibold">Right</span>
-              <span>{data.right ?? 'N/A'}</span>
-            </div>
+          <div className="flex flex-col items-center">
+            <span
+              className={`font-semibold rounded-full h-16 w-16 flex items-center justify-center ${
+                data.right ? "bg-green-500 text-white" : "bg-red-500 text-white"
+              }`}
+            >
+              2
+            </span>
           </div>
-
-          <p className="mt-4 text-[#868386] font-semibold text-sm">Parent: {data.parent}</p>
         </div>
+      </div>
     </div>
   );
 };
